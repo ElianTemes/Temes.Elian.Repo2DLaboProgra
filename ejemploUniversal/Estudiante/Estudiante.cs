7a2ac78ac47d870f4e857a5bde91a63b77ae408e
Elian <eliantemes1303@gmail.com>
2021-08-30 23:21:51 -0300

    Tarea

---
+++ ejemploUniversal/Estudiante/Estudiante.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Estudiantes
+{
+    public class Estudiante
+    {
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random random;
+
+        static Estudiante()
+        {
+            random = new Random();
+        }
+
+        public Estudiante(string apellido, string legajo, string nombre)
+        {
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nombre = nombre;
+        }
+
+        public void SetNotaPrimerParcial(int nota)
+        {
+            this.notaPrimerParcial = nota;
+        }
+        public void SetNotaSegundoParcial(int nota)
+        {
+            this.notaSegundoParcial = nota;
+        }
+
+        private float CalcularPromedio()
+        {
+            return (this.notaPrimerParcial + (float)this.notaSegundoParcial) / 2;
+        }
+        
+        public float CalcularNotaFinal()
+        {
+            if(this.notaPrimerParcial >= 4 && this.notaSegundoParcial >= 4)
+            {
+                return random.Next(6, 11);
+            }
+            return -1;
+        }
+
+        public string Mostrar()
+        {
+
+        }
+    }
+}
