23bc4a87f88dccc6e64f9650c66c83a41908ccfd
Elian <eliantemes1303@gmail.com>
2021-09-20 21:20:09 -0300

    Tarea

---
+++ Sobreescrito/Sobreescrito/Sobrescrito.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobrescribir
+{
+    class Sobrescrito
+    {
+        public override string ToString()
+        {
+            return "¡Este es mi método ToString sobrescrito!";
+        }
+        public override bool Equals(object obj)
+        {
+            return obj.GetType() == typeof(Sobrescrito);
+        }
+        public override int GetHashCode()
+        {
+            return 1142510181;
+        }
+    }
+}
